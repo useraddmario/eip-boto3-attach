8c40a0e4012e2b16b3260671a0608e068c224610
Mario Rodriguez <ec2-user@ip-10-1-0-106.us-west-2.compute.internal>
2020-10-04 08:44:25 +0000

    Type: Initial Development Added EIP association and logging, will need to refactor and add requirements.txt file

--- lifecycle.py
+++ lifecycle.py
@@ -3,11 +3,24 @@
 import boto3
 import json
 import pprint
+import logging
 from botocore.exceptions import ClientError
-
+import subprocess
 
 pp = pprint.PrettyPrinter(indent=4)
 SEARCHTERM = 'Jump'
+
+####Generate a log filr####
+subprocess.run(["touch", "boto3-init.log"])
+
+####Add logging####
+logging.basicConfig(
+    filename='boto3-init.log',
+    level=logging.INFO,
+    format=f'%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger()
+logger.debug('The script is starting.')
+logger.info('Connecting to EC2...')
 
 ####Get Jump EIP AllocationId####
 ec2 = boto3.client('ec2')
@@ -23,11 +36,10 @@ for address in response['Addresses']:
         if SEARCHTERM in each['Value']:
             allocid = address['AllocationId']
 
-print(allocid)
-print('\n###########################\n')
+logger.info(allocid)
 
 
-####Get correct ASG####
+####Get Jump ASG####
 asg = boto3.client('autoscaling')
 response = asg.describe_auto_scaling_groups()
 
@@ -37,7 +49,17 @@ for group in response['AutoScalingGroups']:
     if SEARCHTERM in group['AutoScalingGroupName']:
         instanceid = group['Instances'][0]['InstanceId']
 
-pp.pprint(instanceid)
-print('\n###########################\n')
+logger.info(instanceid)
+
+
+####Associate EIP with Jump Box####
+
+try:
+    response = ec2.associate_address(AllocationId=allocid,
+                                     InstanceId=instanceid)
+    logger.info(response)
+except ClientError as e:
+    logger.info(e)
+
 
 
